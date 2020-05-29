c7feef5e1673c482f666a3665489af04abcbaf96
José Sánchez-Gallego <gallegoj@uw.edu>
2020-05-29 00:55:53 -0600

    Discover carton classes automatically

--- python/target_selection/cartons/__init__.py
+++ python/target_selection/cartons/__init__.py
@@ -1,10 +1,25 @@
 
-# flake8: noqa
-
+import glob
+import importlib
+import inspect
+import os
 
 from .base import BaseCarton
 
 # Import cartons so that they can be discovered by
 # calling Carton.__subclasses__().
-# Eventually maybe automate this with a glob.
-from .guide import GuideCarton
+
+exclusions = ['__init__.py', 'base.py']
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+files = [file_ for file_ in glob.glob('**/*.py', recursive=True)
+         if file_ not in exclusions]
+
+for file_ in files:
+    modname = file_[0:-3].replace('/', '.')
+    mod = importlib.import_module('target_selection.cartons.' + modname)
+    for objname in dir(mod):
+        obj = getattr(mod, objname)
+        if inspect.isclass(obj) and issubclass(obj, BaseCarton):
+            locals().update({objname: obj})
