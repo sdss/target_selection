c67a342b2cd60f0ee3d4b085946d74422f3efb40
José Sánchez-Gallego <gallegoj@uw.edu>
2020-05-31 11:01:51 -0700

    Add query_region to GG

--- python/target_selection/cartons/galactic_genesis.py
+++ python/target_selection/cartons/galactic_genesis.py
@@ -6,6 +6,8 @@
 # @Filename: galactic_genesis.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+import peewee
+
 from sdssdb.peewee.sdss5db.catalogdb import (Catalog, CatalogToTIC_v8,
                                              TIC_v8, TwoMassPSC)
 
@@ -17,7 +19,7 @@ class GalacticGenesisCarton(BaseCarton):
     name = 'galactic_genesis'
     category = 'science'
 
-    def build_query(self, version_id):
+    def build_query(self, version_id, query_region=None):
 
         gg = (TwoMassPSC
               .select(Catalog.catalogid)
@@ -28,5 +30,11 @@ class GalacticGenesisCarton(BaseCarton):
                      ((TwoMassPSC.h_m - TIC_v8.gmag) > self.parameters['h_g'] |
                       TIC_v8.gaia >> None),
                      Catalog.version_id == version_id))
+
+        if query_region:
+            gg = gg.where(peewee.fn.q3c_radial_query(Catalog.ra, Catalog.dec,
+                                                     query_region[0],
+                                                     query_region[1],
+                                                     query_region[2]))
 
         return gg
