f7733c256be1948b259f7f2dae82538f9894aca9
José Sánchez-Gallego <gallegoj@uw.edu>
2020-06-02 00:22:55 -0600

    Add quality flag filtering

--- python/target_selection/cartons/mwm_galactic.py
+++ python/target_selection/cartons/mwm_galactic.py
@@ -8,7 +8,8 @@
 
 import peewee
 
-from sdssdb.peewee.sdss5db.catalogdb import Catalog, CatalogToTIC_v8, TIC_v8
+from sdssdb.peewee.sdss5db.catalogdb import (Catalog, CatalogToTIC_v8,
+                                             TIC_v8, TwoMassPSC)
 
 from . import BaseCarton
 
@@ -21,6 +22,20 @@ class GalacticGenesisCarton(BaseCarton):
     plane and bulge). Select sources brighter than H<11 AND ((G-H) > 3.5 OR
     Gaia non-detection). Approximately 5 million stars.
 
+    Pseudo-query:
+
+        SELECT catalog.catalogid, catalog.ra, catalog.ra, catalog.dec,
+            catalog.pmra, catalog.pmdec, catalog.epoch, twomass_psc.h_m,
+            gaia_dr2_source.phot_g_mean_mag
+        FROM catalog
+        JOIN catalog_to_TIC, TIC, gaia_dr2_source, gaia_clean, twomass_psc
+        WHERE h_m < 11
+            AND (ph_qual[1] = 'A' OR ph_qual[1] = 'B')
+            AND gal_contam=0
+            AND cc_flg[1]=0
+            AND (rd_flg[1] > 0 AND rd_flg[1] <= 3)
+            AND [(phot_g_mean_mag-h_m) > 3.5  OR NOT_EXISTS(phot_g_mean_mag)]
+
     """
 
     name = 'mwm_galactic'
@@ -31,11 +46,26 @@ class GalacticGenesisCarton(BaseCarton):
         Hmag = TIC_v8.hmag
         Gmag = TIC_v8.gaiamag
 
+        ph_qual = TwoMassPSC.ph_qual
+        cc_flg = TwoMassPSC.cc_flg
+        rd_flg = TwoMassPSC.rd_flg
+        rd_flag_1 = peewee.fn.substr(rd_flg, 2, 1).cast('integer')
+
+        gal_contam = TwoMassPSC.gal_contam
+
         gg = (TIC_v8
-              .select(Catalog.catalogid)
-              .join(CatalogToTIC_v8)
+              .select(Catalog.catalogid,
+                      Catalog.ra, Catalog.dec,
+                      Hmag, Gmag,
+                      ph_qual, cc_flg, rd_flg, gal_contam)
+              .join(TwoMassPSC)
+              .join_from(TIC_v8, CatalogToTIC_v8)
               .join(Catalog)
               .where(Hmag < self.parameters['h_max'],
+                     ph_qual.regexp('.(A|B).'),
+                     gal_contam == 0,
+                     peewee.fn.substr(cc_flg, 2, 1) == '0',
+                     rd_flag_1 > 0, rd_flag_1 <= 3,
                      (((Gmag - Hmag) > self.parameters['g_h']) |
                       TIC_v8.gaia >> None),
                      Catalog.version_id == version_id))
