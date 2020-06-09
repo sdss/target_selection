4f4450b2ee0ebe59ca33b2e7a3e6c353ce80da98
José Sánchez-Gallego <gallegoj@uw.edu>
2020-06-09 07:22:13 -0600

    Add back index on target_id

--- python/target_selection/xmatch.py
+++ python/target_selection/xmatch.py
@@ -1115,7 +1115,7 @@ class XMatchPlanner(object):
         class BaseModel(peewee.Model):
 
             catalogid = peewee.BigIntegerField(null=False, index=True)
-            target_id = model_pk_class(null=False)
+            target_id = model_pk_class(null=False, index=True)
             version_id = peewee.SmallIntegerField(null=False, index=True)
             distance = peewee.DoubleField(null=True)
             best = peewee.BooleanField(null=False, index=True)
