ea0d653f1865bee972675fbb82dfa5e032526561
Pramod Gupta <psgupta@astro.washington.edu>
2021-08-25 08:46:32 -0600

    mwm_erosita cartons: add extra cases to if statement in post_process()

--- python/target_selection/cartons/mwm_erosita.py
+++ python/target_selection/cartons/mwm_erosita.py
@@ -208,21 +208,36 @@ class MWM_EROSITA_Stars_Carton(BaseCarton):
             current_g = output[i][1]
             current_h = output[i][2]
 
-            if((current_g < bright_bright_limit) and
-               (current_h is not None) and
-               (current_h < ir_faint_limit)):
-                current_instrument = 'APOGEE'  # This is APOGEE. Others below are BOSS.
-                current_cadence = 'bright_1x1'
-                current_priority = 2400
-            elif((bright_bright_limit < current_g) and (current_g < 17)):
+            # current_g corresponds to gaia_dr2_g which is not null
+            # So we do not check if current_g is None.
+            # However, current_h corresponds to twomass_psc_h_m which may be null.
+            # So we check if current_h is null.
+            if(current_g < bright_bright_limit):
+
+                if(current_h is not None):
+                    if(current_h <= ir_faint_limit):
+                        current_instrument = 'APOGEE'  # This is APOGEE. Others below are BOSS.
+                        current_cadence = 'bright_1x1'
+                        current_priority = 2400
+                    else:
+                        current_instrument = 'BOSS'
+                        current_cadence = 'bright_1x1'
+                        current_priority = 2400
+                else:
+                    # current_h is None
+                    current_instrument = 'BOSS'
+                    current_cadence = 'bright_1x1'
+                    current_priority = 2400
+
+            elif((bright_bright_limit <= current_g) and (current_g < 17)):
                 current_instrument = 'BOSS'
                 current_cadence = 'bright_1x1'
                 current_priority = 2400
-            elif((17 < current_g) and (current_g < 19)):
+            elif((17 <= current_g) and (current_g < 19)):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x2'
                 current_priority = 1920
-            elif(19 < current_g):
+            elif(19 <= current_g):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x3'
                 current_priority = 1920
@@ -452,18 +467,21 @@ class MWM_EROSITA_Compact_Gen_Carton(BaseCarton):
             current_g = output[i][1]
 
             if((current_g < bright_bright_limit)):
-                current_instrument = None
-                current_cadence = None
-                current_priority = None
-            elif((bright_bright_limit < current_g) and (current_g < 17)):
+                # The settings for this case are the same
+                # same as the settings for the next case.
+                # We do this case separately for historical reasons.
                 current_instrument = 'BOSS'
                 current_cadence = 'bright_1x1'
                 current_priority = 2400
-            elif((17 < current_g) and (current_g < 19)):
+            elif((bright_bright_limit <= current_g) and (current_g < 17)):
+                current_instrument = 'BOSS'
+                current_cadence = 'bright_1x1'
+                current_priority = 2400
+            elif((17 <= current_g) and (current_g < 19)):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x2'
                 current_priority = 1910
-            elif(19 < current_g):
+            elif(19 <= current_g):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x3'
                 current_priority = 1910
@@ -660,18 +678,21 @@ class MWM_EROSITA_Compact_Var_Carton(BaseCarton):
             current_g = output[i][1]
 
             if((current_g < bright_bright_limit)):
-                current_instrument = None
-                current_cadence = None
-                current_priority = None
-            elif((bright_bright_limit < current_g) and (current_g < 17)):
+                # The settings for this case are the same
+                # same as the settings for the next case.
+                # We do this case separately for historical reasons.
                 current_instrument = 'BOSS'
                 current_cadence = 'bright_1x1'
                 current_priority = 2400
-            elif((17 < current_g) and (current_g < 19)):
+            elif((bright_bright_limit <= current_g) and (current_g < 17)):
+                current_instrument = 'BOSS'
+                current_cadence = 'bright_1x1'
+                current_priority = 2400
+            elif((17 <= current_g) and (current_g < 19)):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x2'
                 current_priority = 1900
-            elif(19 < current_g):
+            elif(19 <= current_g):
                 current_instrument = 'BOSS'
                 current_cadence = 'dark_1x3'
                 current_priority = 1900
