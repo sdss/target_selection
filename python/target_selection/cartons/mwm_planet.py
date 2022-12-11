06ce93c729476cee89e0e861a5372033242cc6f8
Pramod Gupta <psgupta@astro.washington.edu>
2022-12-11 09:45:51 -0700

    mwm_planet.py: carton mwm_tess_2min

--- python/target_selection/cartons/mwm_planet.py
+++ python/target_selection/cartons/mwm_planet.py
@@ -10,7 +10,7 @@ import numpy
 import peewee
 
 from sdssdb.peewee.sdss5db.catalogdb import (Catalog, CatalogToTIC_v8,
-                                             TESS_TOI_v05, TIC_v8, TwoMassPSC)
+                                             TESS_TOI_v1, TIC_v8, TwoMassPSC)
 
 from target_selection.cartons import BaseCarton
 
@@ -31,18 +31,18 @@ def h2exp(hmag, sn=100, exptime=15.0):
     return (nexp)
 
 
-class MWM_TESS_Planets_Carton(BaseCarton):
+class MWM_TESS_2min_Carton(BaseCarton):
     """TESS Planets.
     Definition:
         All planet-candidate TOIs + Community TOIs (CTIOs), TESS
         Candidate-Target List (CTL) selected by a TIC Priority cut.
         All cut between 7 < H < 12.
 
-    All the candidates are compiled in catalogdb.tess_toi_v05. Note that this list
+    All the candidates are compiled in catalogdb.tess_toi_v1. Note that this list
     contains duplicates on ticid.
     """
 
-    name = 'mwm_tess_planet'
+    name = 'mwm_tess_2min'  #  old name 'mwm_tess_planet'
     program = 'mwm_planet'
     category = 'science'
     mapper = 'MWM'
@@ -52,13 +52,13 @@ class MWM_TESS_Planets_Carton(BaseCarton):
 
     def build_query(self, version_id, query_region=None):
 
-        query = (TESS_TOI_v05
+        query = (TESS_TOI_v1
                  .select(CatalogToTIC_v8.catalogid,
-                         TESS_TOI_v05.ticid,
-                         TESS_TOI_v05.tess_disposition,
+                         TESS_TOI_v1.ticid,
+                         TESS_TOI_v1.tess_disposition,
                          TwoMassPSC.h_m.alias('hmag'),
-                         TESS_TOI_v05.target_type.alias('tess_target_type'))
-                 .join(TIC_v8, on=(TESS_TOI_v05.ticid == TIC_v8.id))
+                         TESS_TOI_v1.target_type.alias('tess_target_type'))
+                 .join(TIC_v8, on=(TESS_TOI_v1.ticid == TIC_v8.id))
                  .join(TwoMassPSC, on=(TIC_v8.twomass_psc == TwoMassPSC.designation))
                  .switch(TIC_v8)
                  .join(CatalogToTIC_v8, on=(CatalogToTIC_v8.target_id == TIC_v8.id))
@@ -66,7 +66,7 @@ class MWM_TESS_Planets_Carton(BaseCarton):
                         CatalogToTIC_v8.best >> True,
                         TwoMassPSC.h_m > 7,
                         TwoMassPSC.h_m < 12)
-                 .distinct([TESS_TOI_v05.ticid]))
+                 .distinct([TESS_TOI_v1.ticid]))
 
         if query_region:
             query = (query
@@ -81,8 +81,8 @@ class MWM_TESS_Planets_Carton(BaseCarton):
 
     def post_process(self, model):
 
-        # priorities = {'exo_TOI': 2600,
-        #               'exo_CTOI': 2605,
+        # priorities = {'exo_TOI': 2610,
+        #               'exo_CTOI': 2610,
         #               '2min': 2610}
 
         cursor = self.database.execute_sql(
@@ -96,10 +96,11 @@ class MWM_TESS_Planets_Carton(BaseCarton):
             current_hmag = output[i][1]
             current_tess_target_type = str(output[i][2]).strip()
 
+            # For all, current_priority is the same i.e. 2610
             if (current_tess_target_type == 'exo_TOI'):
-                current_priority = 2600
+                current_priority = 2610
             elif (current_tess_target_type == 'exo_CTOI'):
-                current_priority = 2605
+                current_priority = 2610
             elif (current_tess_target_type == '2min'):
                 current_priority = 2610
             else:
