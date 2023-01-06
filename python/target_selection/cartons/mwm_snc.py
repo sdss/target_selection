206f5e52794c6e12c5efe26ba43bad1812ad66f6
José Sánchez-Gallego <gallegoj@uw.edu>
2023-01-06 13:20:13 -0800

    Update SNC cartons for v1

--- python/target_selection/cartons/mwm_snc.py
+++ python/target_selection/cartons/mwm_snc.py
@@ -8,8 +8,13 @@
 
 from peewee import fn
 
-from sdssdb.peewee.sdss5db.catalogdb import (Catalog, CatalogToTIC_v8,
-                                             Gaia_DR2, TIC_v8)
+from sdssdb.peewee.sdss5db.catalogdb import (
+    Catalog,
+    CatalogToGaia_DR3,
+    CatalogToTwoMassPSC,
+    Gaia_DR3,
+    TwoMassPSC
+)
 
 from target_selection.cartons import BaseCarton
 
@@ -49,35 +54,46 @@ class MWM_SNC_100pc_Carton(BaseCarton):
 
     def build_query(self, version_id, query_region=None):
 
-        l = Gaia_DR2.l  # noqa
-        b = Gaia_DR2.b
+        ll = Gaia_DR3.l
+        bb = Gaia_DR3.b
 
         # Dense regions (Galactic plane, SMC, LMC).
-        gal_cut = (((l <= 180) & (b < (-0.139 * l + 25)) & (b > (0.139 * l - 25))) |  # noqa
-                   ((l > 180) & (b > (-0.139 * l + 25)) & (b < (0.139 * l - 25))) |
-                   (fn.sqrt(fn.pow(l - 303.2, 2) + 2 * fn.pow(b + 44.4, 2)) < 5) |
-                   (fn.sqrt(fn.pow(l - 280.3, 2) + 2 * fn.pow(b + 33.0, 2)) < 8))
+        gal_cut = (
+            ((ll <= 180) & (bb < (-0.139 * ll + 25)) & (bb > (0.139 * ll - 25)))
+            | ((ll > 180) & (bb > (-0.139 * ll + 25)) & (bb < (0.139 * ll - 25)))
+            | (fn.sqrt(fn.pow(ll - 303.2, 2) + 2 * fn.pow(bb + 44.4, 2)) < 5)
+            | (fn.sqrt(fn.pow(ll - 280.3, 2) + 2 * fn.pow(bb + 33.0, 2)) < 8)
+        )
 
-        query = (Gaia_DR2
-                 .select(CatalogToTIC_v8.catalogid,
-                         TIC_v8.gaia_int.alias('gaia_source_id'),
-                         Gaia_DR2.phot_g_mean_mag,
-                         l, b)
-                 .join(TIC_v8)
-                 .join(CatalogToTIC_v8)
-                 .where((Gaia_DR2.parallax - Gaia_DR2.parallax_error) > 10,
-                        ((Gaia_DR2.astrometric_excess_noise < 2) & gal_cut) | ~(gal_cut))
-                 .where(CatalogToTIC_v8.version_id == version_id,
-                        CatalogToTIC_v8.best >> True))
+        query = (
+            Gaia_DR3.select(
+                CatalogToGaia_DR3.catalogid,
+                Gaia_DR3.source_id,
+                Gaia_DR3.phot_g_mean_mag,
+                ll,
+                bb,
+            )
+            .join(CatalogToGaia_DR3)
+            .where(
+                (Gaia_DR3.parallax - Gaia_DR3.parallax_error) > 10,
+                ((Gaia_DR3.astrometric_excess_noise < 2) & gal_cut) | ~(gal_cut),
+            )
+            .where(
+                CatalogToGaia_DR3.version_id == version_id,
+                CatalogToGaia_DR3.best >> True,
+            )
+        )
 
         if query_region:
-            query = (query
-                     .join_from(CatalogToTIC_v8, Catalog)
-                     .where(fn.q3c_radial_query(Catalog.ra,
-                                                Catalog.dec,
-                                                query_region[0],
-                                                query_region[1],
-                                                query_region[2])))
+            query = query.join_from(CatalogToGaia_DR3, Catalog).where(
+                fn.q3c_radial_query(
+                    Catalog.ra,
+                    Catalog.dec,
+                    query_region[0],
+                    query_region[1],
+                    query_region[2],
+                )
+            )
 
         return query
 
@@ -96,8 +112,15 @@ class MWM_SNC_100pc_APOGEE_Carton(MWM_SNC_100pc_Carton):
     def build_query(self, version_id, query_region=None):
 
         query = super().build_query(version_id, query_region=query_region)
+        query.switch(CatalogToGaia_DR3).join(
+            CatalogToTwoMassPSC,
+            on=(CatalogToGaia_DR3.catalogid == CatalogToTwoMassPSC.catalogid),
+        ).join(TwoMassPSC).where(
+            TwoMassPSC.h_m < 11,
+            CatalogToTwoMassPSC.version_id == version_id,
+            CatalogToTwoMassPSC.best >> True)
 
-        return query.where(TIC_v8.hmag < 11)
+        return query
 
 
 class MWM_SNC_100pc_BOSS_Carton(MWM_SNC_100pc_Carton):
@@ -112,118 +135,8 @@ class MWM_SNC_100pc_BOSS_Carton(MWM_SNC_100pc_Carton):
 
     def post_process(self, model, **kwargs):
 
-        # G > 16 => cadence = dark_2x1
-        model.update(cadence='dark_2x1').where(model.phot_g_mean_mag > 16).execute()
-
-        # G < 16 => cadence = bright_2x1
-        model.update(cadence='bright_2x1').where(model.phot_g_mean_mag < 16).execute()
-
-        return model
-
-
-class MWM_SNC_250pc_Carton(BaseCarton):
-    """MWM Solar Neighbourhood Census 250pc.
-
-    Definition:
-
-        - Divide the sky in crowded/not crowded regions via simple geometric
-          cuts in (l,b)
-        - Apply a general cut of absolute G-band magnitude < 6mag,
-          corresponding to ~G-type stars and earlier
-        - Select all stars that fall within their one-sigma uncertainty
-          within 250pc in the not crowded regions
-        - Select all stars that fall within their one-sigma uncertainty within
-          250pc and have an astrometric_excess_noise < 2 in the crowded
-          regions.
-
-    Pseudo SQL:
-
-        (phot_g_mean_mag + 5 * log10(parallax / 1000) + 5 < 6) &&
-        parallax - parallax_error > 4 &&
-        ( (astrometric_excess_noise < 2 &&
-           ( (l <= 180 && b < -0.139 * l + 25 && b > 0.139 * l - 25) ||
-             (l > 180 && b > -0.139 * l + 25 && b < 0.139 * l - 25) ||
-             (sqrt(pow(l - 303.2, 2) + 2 * pow(b + 44.4, 2)) < 5) ||
-             (sqrt(pow(l - 280.3, 2) + 2 * pow(b + 33.0, 2)) < 8))) ||
-          !( (l <= 180 && b < -0.139 * l + 25 && b > 0.139 * l - 25) ||
-             (l > 180 && b > -0.139 * l + 25 && b < 0.139 * l - 25) ||
-             (sqrt(pow(l - 303.2, 2) + 2 * pow(b + 44.4, 2)) < 5) ||
-             (sqrt(pow(l - 280.3, 2) + 2 * pow(b + 33.0, 2)) < 8)
-            )
-        )
-
-    This is a base carton. Actual cartons are implemented as subclasses for the
-    different magnitude cuts.
-
-    """
-
-    def build_query(self, version_id, query_region=None):
-
-        l = Gaia_DR2.l  # noqa
-        b = Gaia_DR2.b
-
-        colour_cut = (Gaia_DR2.phot_g_mean_mag +
-                      5 * fn.log(Gaia_DR2.parallax / 1000) + 5) < 6
-
-        gal_cut = (((l <= 180) & (b < (-0.139 * l + 25)) & (b > (0.139 * l - 25))) |  # noqa
-                   ((l > 180) & (b > (-0.139 * l + 25)) & (b < (0.139 * l - 25))) |
-                   (fn.sqrt(fn.pow(l - 303.2, 2) + 2 * fn.pow(b + 44.4, 2)) < 5) |
-                   (fn.sqrt(fn.pow(l - 280.3, 2) + 2 * fn.pow(b + 33.0, 2)) < 8))
-
-        query = (Gaia_DR2
-                 .select(CatalogToTIC_v8.catalogid,
-                         TIC_v8.gaia_int.alias('gaia_source_id'),
-                         Gaia_DR2.phot_g_mean_mag,
-                         l, b)
-                 .join(TIC_v8)
-                 .join(CatalogToTIC_v8)
-                 .where(colour_cut,
-                        (Gaia_DR2.parallax - Gaia_DR2.parallax_error) > 4,
-                        ((Gaia_DR2.astrometric_excess_noise < 2) & gal_cut) | ~(gal_cut))
-                 .where(CatalogToTIC_v8.version_id == version_id,
-                        CatalogToTIC_v8.best >> True))
-
-        if query_region:
-            query = (query
-                     .join_from(CatalogToTIC_v8, Catalog)
-                     .where(fn.q3c_radial_query(Catalog.ra,
-                                                Catalog.dec,
-                                                query_region[0],
-                                                query_region[1],
-                                                query_region[2])))
-
-        return query
-
-
-class MWM_SNC_250pc_APOGEE_Carton(MWM_SNC_250pc_Carton):
-    """MWM SNC 250pc targets to be observed with APOGEE."""
-
-    name = 'mwm_snc_250pc_apogee'
-    category = 'science'
-    program = 'mwm_snc'
-    mapper = 'MWM'
-    instrument = 'APOGEE'
-    cadence = 'bright_1x1'
-    priority = 1815
-
-    def build_query(self, version_id, query_region=None):
-
-        query = super().build_query(version_id, query_region=query_region)
-
-        return query.where(TIC_v8.hmag < 11)
-
-
-class MWM_SNC_250pc_BOSS_Carton(MWM_SNC_250pc_Carton):
-    """MWM SNC 250pc targets to be observed with BOSS."""
-
-    name = 'mwm_snc_250pc_boss'
-    category = 'science'
-    program = 'mwm_snc'
-    mapper = 'MWM'
-    instrument = 'BOSS'
-    priority = 1810
-
-    def post_process(self, model, **kwargs):
+        # Minimum magnitude cut.
+        model = model.where(model.phot_g_mean_mag > 10)
 
         # G > 16 => cadence = dark_2x1
         model.update(cadence='dark_2x1').where(model.phot_g_mean_mag > 16).execute()
