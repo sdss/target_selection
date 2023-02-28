9ad42176b6c782b16579a39d53714f7ad8c0efbf
Pramod Gupta <psgupta@astro.washington.edu>
2023-02-28 10:10:15 -0700

    mwm_galactic.py: edit mwm_galactic_core_dist

--- python/target_selection/cartons/mwm_galactic.py
+++ python/target_selection/cartons/mwm_galactic.py
@@ -94,9 +94,9 @@ class MWM_Galactic_Core_Carton(BaseCarton):
                          Gaia_DR3.dec,
                          Gaia_DR3.pmra,
                          Gaia_DR3.pmdec,
+                         Gaia_DR3.phot_g_mean_mag,
                          TwoMassPSC.h_m,
-                         TwoMassPSC.designation,
-                         Gaia_DR3.phot_g_mean_mag)
+                         TwoMassPSC.designation)
                  .join(TwoMassPSC,
                        on=(CatalogToTwoMassPSC.target_id == TwoMassPSC.pts_key))
                  .switch(CatalogToTwoMassPSC)
@@ -115,6 +115,136 @@ class MWM_Galactic_Core_Carton(BaseCarton):
                         (peewee.fn.substr(TwoMassPSC.rd_flg, 2, 1).cast('integer') <= 3),
                         ((Gaia_DR3.phot_g_mean_mag - TwoMassPSC.h_m) > 3.5) |
                         (Gaia_DR3.phot_g_mean_mag >> None)))
+        # above condition (Gaia_DR3.phot_g_mean_mag >> None) ensures that
+        # that we get the rows from the left outer join
+
+        if query_region:
+            query = (query
+                     .join_from(CatalogToTwoMassPSC, Catalog)
+                     .where(peewee.fn.q3c_radial_query(Catalog.ra, Catalog.dec,
+                                                       query_region[0],
+                                                       query_region[1],
+                                                       query_region[2])))
+
+        return query
+
+
+class MWM_Galactic_Core_Dist_Carton(BaseCarton):
+    """Galactic Genesis carton.
+
+    Definition: Selection of all IR-bright, red stars – vast majority are red
+    giants; follows the density distribution of the MW (concentrated in the
+    plane and bulge). Select sources brighter than H<11 AND ((G-H) > 3.5 OR
+    Gaia non-detection). Approximately 5 million stars.
+
+    v0.5
+    Pseudo-query:
+
+        SELECT catalog.catalogid, catalog.ra, catalog.ra, catalog.dec,
+            catalog.pmra, catalog.pmdec, catalog.epoch, twomass_psc.h_m,
+            gaia_dr2_source.phot_g_mean_mag
+        FROM catalog
+        JOIN catalog_to_TIC, TIC, gaia_dr2_source, gaia_clean, twomass_psc
+        WHERE h_m < 11
+            AND (ph_qual[1] = 'A' OR ph_qual[1] = 'B')
+            AND gal_contam=0
+            AND cc_flg[1]=0
+            AND (rd_flg[1] > 0 AND rd_flg[1] <= 3)
+            AND [(phot_g_mean_mag-h_m) > 3.5  OR NOT_EXISTS(phot_g_mean_mag)]
+
+    v1.0
+    Shorthand name: mwm_galactic_core
+    Existing carton code:
+    https://github.com/sdss/target_selection/blob/main/python/target_selection/cartons/mwm_galactic.py
+
+    Simplified Description of selection criteria:
+    Selection of all IR-bright, red stars – vast majority are red giants;
+     follows the density distribution of the MW (concentrated in the plane and bulge).
+     Select sources brighter than H<11 AND ((G-H) > 3.5 OR Gaia non-detection).
+    Gaia DR2 parameters to be converted to Gaia DR3: position information, phot_g_mean_mag,
+
+    Pseudo-code: Same as v0.5
+
+    SELECT catalog.catalogid, gaia_dr3.source_id, gaia_dr3.ra,  gaia_dr3.dec,
+        gaia_dr3.pmra, gaia_dr3.pmdec,  twomass_psc.h_m, twomass_psc.designation,
+        gaia_dr3.phot_g_mean_mag
+    FROM catalog JOIN catalog_to_twomass_psc, catalog_to_gaia_dr3,  gaia_dr3,  twomass_psc
+    WHERE
+    h_m < 11 AND
+    (ph_qual[1] = 'A' OR ph_qual[1] = 'B') AND
+    gal_contam=0 AND
+    cc_flg[1]=0 AND
+    (rd_flg[1] > 0 AND rd_flg[1] <= 3)
+    AND [(phot_g_mean_mag-h_m) > 3.5 OR NOT_EXISTS(phot_g_mean_mag)]
+
+    Above  is same as for mwm_galactic_core
+    Below is additional for mwm_galacic_core_dist
+    tmass.h_m < 11 AND
+    (
+
+    ( gaia.parallax < 100 * power(10, 0.2 * (-2.5 - tmass.h_m)) ) OR
+    (g.phot_g_mean_mag - tmass.h_m > 5) OR
+    (NO gaia.phot_g_mean_mag)
+    )
+
+    Joins: TwoMassPSC, catalog_to_twomass_psc, catalog_to_gaia_dr3, gaia_dr3
+
+    Return columns: Gaia E/DR3 source_id, ra, dec, parallax,
+     Gaia E/DR3 G mag, Gaia E/DR3 BP mag, Gaia E/DR3 RP mag, J mag,
+      H mag K mag, catalogid, 2MASS designation, 2MASS ph_qual, cc_flg, rd_flg, gal_contam
+
+    Metadata:
+    unchanged
+    can_offset=TRUE
+    Lead contact: Jonathan Bird
+
+    """
+
+    name = 'mwm_galactic_core_dist'
+    category = 'science'
+    instrument = 'APOGEE'
+    cadence = 'bright_1x1'
+    priority = 2710
+    program = 'mwm_galactic'
+    mapper = 'MWM'
+    can_offset = True
+
+    def build_query(self, version_id, query_region=None):
+
+        query = (CatalogToTwoMassPSC
+                 .select(CatalogToTwoMassPSC.catalogid,
+                         Gaia_DR3.source_id,
+                         Gaia_DR3.ra,
+                         Gaia_DR3.dec,
+                         Gaia_DR3.pmra,
+                         Gaia_DR3.pmdec,
+                         Gaia_DR3.parallax,
+                         Gaia_DR3.phot_g_mean_mag,
+                         TwoMassPSC.h_m,
+                         TwoMassPSC.designation)
+                 .join(TwoMassPSC,
+                       on=(CatalogToTwoMassPSC.target_id == TwoMassPSC.pts_key))
+                 .switch(CatalogToTwoMassPSC)
+                 .join(CatalogToGaia_DR3, peewee.JOIN.LEFT_OUTER,
+                       on=(CatalogToTwoMassPSC.catalogid == CatalogToGaia_DR3.catalogid))
+                 .join(Gaia_DR3, peewee.JOIN.LEFT_OUTER,
+                       on=(CatalogToGaia_DR3.target_id == Gaia_DR3.source_id))
+                 .where(CatalogToTwoMassPSC.version_id == version_id,
+                        CatalogToTwoMassPSC.best >> True,
+                        TwoMassPSC.h_m < 11,
+                        (peewee.fn.substr(TwoMassPSC.ph_qual, 2, 1) == 'A') |
+                        (peewee.fn.substr(TwoMassPSC.ph_qual, 2, 1) == 'B'),
+                        TwoMassPSC.gal_contam == 0,
+                        peewee.fn.substr(TwoMassPSC.cc_flg, 2, 1) == '0',
+                        (peewee.fn.substr(TwoMassPSC.rd_flg, 2, 1).cast('integer') > 0) &
+                        (peewee.fn.substr(TwoMassPSC.rd_flg, 2, 1).cast('integer') <= 3),
+                        ((Gaia_DR3.phot_g_mean_mag - TwoMassPSC.h_m) > 3.5) |
+                        (Gaia_DR3.phot_g_mean_mag >> None),
+                        (Gaia_DR3.parallax <
+                         100 * peewee.fn.power(10, 0.2 * (-2.5 - TwoMassPSC.h_m))) |
+                        ((Gaia_DR3.phot_g_mean_mag - TwoMassPSC.h_m) > 5) |
+                        (Gaia_DR3.phot_g_mean_mag >> None)))
+
         # above condition (Gaia_DR3.phot_g_mean_mag >> None) ensures that
         # that we get the rows from the left outer join
 
