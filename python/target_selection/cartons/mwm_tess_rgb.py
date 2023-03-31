650a29b3d540da7e1c5b1f3382a854d19a8a80ca
Pramod Gupta <psgupta@astro.washington.edu>
2023-03-31 17:21:56 -0600

    edits for tag1.0.11

--- python/target_selection/cartons/mwm_tess_rgb.py
+++ python/target_selection/cartons/mwm_tess_rgb.py
@@ -3,7 +3,7 @@
 #
 # @Author: José Sánchez-Gallego (gallegoj@uw.edu)
 # @Date: 2020-07-01
-# @Filename: mwm_cb_uvex.py
+# @Filename: mwm_tess_rgb.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
 import numpy
