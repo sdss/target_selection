6a4408874e62def130aa9ca1407155197a68bc33
Pramod Gupta <61213683+astronomygupta@users.noreply.github.com>
2023-04-29 14:47:41 -0700

    edits for mwm_halo_gaia_dr3.py (#317)

--- python/target_selection/cartons/mwm_halo_gaia_dr3.py
+++ python/target_selection/cartons/mwm_halo_gaia_dr3.py
@@ -165,7 +165,7 @@ Link to paper: https://arxiv.org/abs/2302.02611 and
 Zenodo: https://doi.org/10.5281/zenodo.7599789
 Use catalogdb.xpfeh_gaia_dr3.
 
-Cut on BP<17, teff_xgboost < 5500, logg_xgboost < 4, mh_xgboost < 0
+Cut on BP<17, teff_xgboost < 5500, logg_xgboost < 4, mh_xgboost < -1.0
 w1mpro (from AllWise) absolute magnitude.
 
 Specifically:
@@ -175,7 +175,7 @@ with teff_xgboost on right hand side)
 BP < 17
 logg_xgboost < 4.0
 teff_xgboost < 5500
-mh_xgboost < 0
+mh_xgboost < -1.0
 M_W1 > -0.3 - 0.006 * (5500 - teff_xgboost)
 M_W1 > -0.01 * (5300 - teff_xgboost)
 
@@ -183,7 +183,7 @@ where M_W1 = w1mpro + 5 log10(parallax/100)
 (note: solve these equations so that this is a cut on parallax).
 
 Then there are three levels of priority based on selecting
-mh_xgboost <= -2.0, -2.0 < mh_xgboost <= -1.5, -1.5 < mh_xgboost <= 0
+mh_xgboost <= -2.0, -2.0 < mh_xgboost <= -1.5, -1.5 < mh_xgboost <= -1.0
 at three different priorities.
 
 Return columns:
@@ -191,7 +191,7 @@ Metadata:
 Priority: three levels
 2100 if mh_xgboost <= -2.0 (TBA pending A/B test)
 2970 if -2.0 < mh_xgboost <= -1.5
-6090 if -1.5 < mh_xgboost <= 0
+6090 if -1.5 < mh_xgboost <= -1.0
 Cadence: bright_1x1
 Instrument: BOSS for G>13, APOGEE for G<13
 can_offset = True
@@ -244,7 +244,7 @@ Lead contact: Alexander Ji, Rene Andrae
                         Gaia_DR3.parallax > 0,
                         Xpfeh_gaia_dr3.logg_xgboost < 4.0,
                         Xpfeh_gaia_dr3.teff_xgboost < 5500,
-                        Xpfeh_gaia_dr3.mh_xgboost < 0,
+                        Xpfeh_gaia_dr3.mh_xgboost < -1.0,
                         m_w1 > -0.3 - 0.006 * (5500 - Xpfeh_gaia_dr3.teff_xgboost),
                         m_w1 > -0.01 * (5300 - Xpfeh_gaia_dr3.teff_xgboost)))
 
@@ -267,7 +267,7 @@ Lead contact: Alexander Ji, Rene Andrae
         Priority: three levels
         2100 if mh_xgboost <= -2.0 (TBA pending A/B test)
         2970 if -2.0 < mh_xgboost <= -1.5
-        6090 if -1.5 < mh_xgboost <= 0
+        6090 if -1.5 < mh_xgboost <= -1.0
 
         Instrument: BOSS for G>13, APOGEE for G<13
         """
@@ -283,6 +283,7 @@ Lead contact: Alexander Ji, Rene Andrae
             current_mh_xgboost = output[i][1]
             current_phot_g_mean_mag = output[i][2]
 
+            # From query in build_query(), we have mh_xgboost < -1.0
             if (current_mh_xgboost <= -2.0):
                 current_priority = 2100
             elif ((current_mh_xgboost > -2.0) and
@@ -319,7 +320,7 @@ Link to paper: https://arxiv.org/abs/2302.02611 and
 Zenodo: https://doi.org/10.5281/zenodo.7599789
 Use catalogdb.xpfeh_gaia_dr3.
 
-Cut on BP<17, teff_xgboost < 5500, logg_xgboost < 4, mh_xgboost < 0
+Cut on BP<17, teff_xgboost < 5500, logg_xgboost < 4, mh_xgboost < -1.0
 w1mpro (from AllWise) absolute magnitude.
 
 Specifically:
@@ -329,7 +330,7 @@ with teff_xgboost on right hand side)
 BP < 17
 logg_xgboost < 4.0
 teff_xgboost < 5500
-mh_xgboost < 0
+mh_xgboost < -1.0
 M_W1 > -0.3 - 0.006 * (5500 - teff_xgboost)
 M_W1 > -0.01 * (5300 - teff_xgboost)
 
@@ -337,14 +338,14 @@ where M_W1 = w1mpro + 5 log10(parallax/100)
 (note: solve these equations so that this is a cut on parallax).
 
 Then there are three levels of priority based on selecting
-mh_xgboost <= -2.0, -2.0 < mh_xgboost <= -1.5, -1.5 < mh_xgboost <= 0.0
+mh_xgboost <= -2.0, -2.0 < mh_xgboost <= -1.5, -1.5 < mh_xgboost <= -1.0
 at three different priorities.
 Return columns:
 Metadata:
 Priority:
 2099 if mh_xgboost <= -2.0 (TBA pending A/B test)
 2969 if -2.0 < mh_xgboost <= -1.5
-6090 if -1.5 < mh_xgboost <= 0
+6090 if -1.5 < mh_xgboost <= -1.0
 Cadence: dark_1x2
 Instrument: BOSS for G>13, APOGEE for G<13
 can_offset = True
@@ -397,7 +398,7 @@ Lead contact: Alexander Ji, Rene Andrae
                         Gaia_DR3.parallax > 0,
                         Xpfeh_gaia_dr3.logg_xgboost < 4.0,
                         Xpfeh_gaia_dr3.teff_xgboost < 5500,
-                        Xpfeh_gaia_dr3.mh_xgboost < 0,
+                        Xpfeh_gaia_dr3.mh_xgboost < -1.0,
                         m_w1 > -0.3 - 0.006 * (5500 - Xpfeh_gaia_dr3.teff_xgboost),
                         m_w1 > -0.01 * (5300 - Xpfeh_gaia_dr3.teff_xgboost)))
 
@@ -420,7 +421,7 @@ Lead contact: Alexander Ji, Rene Andrae
         Priority: three levels
         2099 if mh_xgboost <= -2.0 (TBA pending A/B test)
         2969 if -2.0 < mh_xgboost <= -1.5
-        6090 if -1.5 < mh_xgboost <=0
+        6090 if -1.5 < mh_xgboost <= -1.0
 
         Instrument: BOSS for G>13, APOGEE for G<13
         """
@@ -436,6 +437,7 @@ Lead contact: Alexander Ji, Rene Andrae
             current_mh_xgboost = output[i][1]
             current_phot_g_mean_mag = output[i][2]
 
+            # From query in build_query(), we have mh_xgboost < -1.0
             if (current_mh_xgboost <= -2.0):
                 current_priority = 2099
             elif ((current_mh_xgboost > -2.0) and
