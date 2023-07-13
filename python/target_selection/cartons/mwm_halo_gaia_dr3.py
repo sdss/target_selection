4bca9d9208fa3a049c9196ea34e2d108331a05b3
Pramod Gupta <psgupta@astro.washington.edu>
2023-07-13 12:02:43 -0600

    mwm_halo_gaia_dr3.py: edit mwm_halo_local_vtanhigh_apogee_single

--- python/target_selection/cartons/mwm_halo_gaia_dr3.py
+++ python/target_selection/cartons/mwm_halo_gaia_dr3.py
@@ -464,16 +464,12 @@ Lead contact: Alexander Ji, Rene Andrae
                     " where catalogid = " + str(current_catalogid) + ";")
 
 
-class MWM_halo_local_Carton(BaseCarton):
-    """  5.1.36.  mwm_halo_local
+class MWM_halo_local_Base_Carton(BaseCarton):
+    """
+    This base carton contains the first part of the below query.
+    which is common to all the mmw_halo_local* cartons
+    5.1.36.  mwm_halo_local
 Shorthand name: mwm_halo_local
-Existing carton code: N/A
-
-Note: The mwm_halo_local and mwm_halo_local_dark  cartons differ
-only in cadence (bright_1x1 vs dark_1x2) and
-the numerical value of the priority.
-The structure of the if statements for the priority is the same for both.
-For mwm_halo_local_dark, the priority is reduced by 1 (i.e. a bit higher priority) in all cases.
 
 Simplified Description of selection criteria: Gaia DR3 vtan > 150 km/s,
 parallax_over_error > 5, parallax > 0.5, RP < 20, RUWE < 1.4,
@@ -482,22 +478,105 @@ and parallax_over_error.
 Return columns:
 Metadata:
 
-Priority: 11 levels
-vtan >= 200 and 3 < M_G < 5 and parallax_over_error >= 50: 2980
-200 > vtan >= 150 and 3 < M_G < 5 and parallax_over_error >= 50: 2985
-vtan >= 200 and not (3 < M_G < 5) and parallax_over_error >= 50: 2990
-200 > vtan >= 150 and not (3 < M_G < 5) and parallax_over_error >= 50: 2995
-vtan >= 200 and 3 < M_G < 5 and 50 > parallax_over_error >= 10: 3020
-200 > vtan >= 150 and 3 < M_G < 5 and 50 > parallax_over_error >= 10: 3025
-vtan >= 150 and 3 < M_G < 5 and 10 > parallax_over_error >= 5: 3030
-vtan >= 200 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10: 3040
-200 > vtan >= 150 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10: 3045
-vtan >= 200 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5: 6092
-200 > vtan >= 150 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5: 6094
+mwm_halo_local_vtanhigh_apogee_single
+mwm_halo_local_vtanhigh_boss_single
+mwm_halo_local_vtanlow_boss_single
+mwm_halo_local_vtanlow_apogee_single
+mwm_halo_local_vtanhigh_apogee
+mwm_halo_local_vtanhigh_boss
+mwm_halo_local_vtanlow_boss
+mwm_halo_local_vtanlow_apogee
+Existing carton code: mwm_halo_local ← this forms the base catalog.
+The individual cartons below just change priority and cadence and instrument.
+Note: The mwm_halo_local_single and mwm_halo_local cartons differ only in cadence
+(bright_1x1 vs dark_1x2) and the numerical value of the priority.
+The structure of the if statements for the priority is the same for both.
+For mwm_halo_local, the priority is reduced by 1 (i.e. a bit higher priority)
+in all cases.
+Simplified Description of selection criteria:
+Gaia DR3 vtan > 150 km/s, parallax_over_error > 5, parallax > 0.5, RP < 20,
+RUWE < 1.4, with priority levels based on vtan,
+absolute G mag (prioritizing MSTO stars), and parallax_over_error.
 
-Cadence: bright_1x1
-Instrument: BOSS for G>13, APOGEE for G<13
+Details here: Development of Pseudocode for v1.0 cartons#LocalHalo
+(note: consolidated the 11 priorities into 2)
+
+mwm_halo_local_vtanhigh_apogee_single
+vtan >= 150 and parallax_over_error >=10 OR
+vtan >=150 and 3 < M_G < 5 and 10 > parallax_over_error > 5
+G < 13
+PRIORITY: 2981
+CADENCE: bright_1x1
+INSTRUMENT: APOGEE
 can_offset = True
+
+mwm_halo_local_vtanhigh_boss_single
+vtan >= 150 and parallax_over_error >=10 OR
+vtan >=150 and 3 < M_G < 5 and 10 > parallax_over_error > 5
+G >= 13
+PRIORITY: 2981
+CADENCE: bright_1x1
+INSTRUMENT: BOSS
+can_offset = True
+
+mwm_halo_local_vtanlow_apogee_single
+vtan >=150 and not (3 < M_G < 5) and 10 > parallax_over_error > 5
+G < 13
+PRIORITY: 6501
+CADENCE: bright_1x1
+INSTRUMENT: APOGEE
+can_offset = True
+
+mwm_halo_local_vtanlow_boss_single
+vtan >=150 and not (3 < M_G < 5) and 10 > parallax_over_error > 5
+G >= 13
+PRIORITY: 6501
+CADENCE: bright_1x1
+INSTRUMENT: BOSS
+can_offset = True
+
+mwm_halo_local_vtanhigh_apogee
+vtan >= 150 and parallax_over_error >=10 OR
+vtan >=150 and 3 < M_G < 5 and 10 > parallax_over_error > 5
+G < 13
+PRIORITY: 2980
+CADENCE: dark_flexible_2x1
+INSTRUMENT: APOGEE
+can_offset = True
+
+mwm_halo_local_vtanhigh_boss
+vtan >= 150 and parallax_over_error >=10 OR
+vtan >=150 and 3 < M_G < 5 and 10 > parallax_over_error > 5
+G >= 13
+PRIORITY: 2980
+CADENCE: dark_flexible_2x1
+INSTRUMENT: BOSS
+can_offset = True
+
+mwm_halo_local_vtanlow_apogee
+vtan >=150 and not (3 < M_G < 5) and 10 > parallax_over_error > 5
+G < 13
+PRIORITY: 6501
+CADENCE: dark_flexible_2x1
+INSTRUMENT: APOGEE
+can_offset = True
+
+mwm_halo_local_vtanlow_apogee
+vtan >=150 and not (3 < M_G < 5) and 10 > parallax_over_error > 5
+G < 13
+PRIORITY: 6500
+CADENCE: dark_flexible_2x1
+INSTRUMENT: APOGEE
+can_offset = True
+
+mwm_halo_local_vtanlow_boss
+vtan >=150 and not (3 < M_G < 5) and 10 > parallax_over_error > 5
+G >= 13
+PRIORITY: 6500
+CADENCE: dark_flexible_2x1
+INSTRUMENT: BOSS
+can_offset = True
+
 Lead contact: Alexander Ji, Sebastien Lepine
 
 Details:
@@ -519,31 +598,8 @@ phot_rp_mean_mag < 20. and RUWE < 1.4
 and
 vtan > 150
 
-Priority algorithm: (exact numbers are TBA, this is based on v0.5 priorities)
-LH_MSTO1 priority 2980: vtan >= 200      and 3 < M_G < 5       and parallax_over_error >= 50 (N=14k)  # noqa: E501
-LH_MSTO2 priority 2985: 200 > vtan >= 150 and 3 < M_G < 5       and parallax_over_error >= 50 (N=24k)  # noqa: E501
-LH_ALL1  priority 2990: vtan >= 200      and not (3 < M_G < 5) and parallax_over_error >= 50 (N=28k)  # noqa: E501
-LH_ALL2  priority 2995: 200 > vtan >= 150 and not (3 < M_G < 5) and parallax_over_error >= 50 (N=42k)  # noqa: E501
-LH_MSTO3 priority 3020: vtan >= 200      and 3 < M_G < 5       and 50 > parallax_over_error >= 10 (N=98k)  # noqa: E501
-LH_MSTO4 priority 3025: 200 > vtan >= 150 and 3 < M_G < 5       and 50 > parallax_over_error >= 10 (N=179k)  # noqa: E501
-LH_MSTO5 priority 3030: vtan >= 150       and 3 < M_G < 5       and 10 > parallax_over_error >= 5 (N=12k)  # noqa: E501
-LH_ALL3  priority 3040: vtan >= 200      and not (3 < M_G < 5) and 50 > parallax_over_error >= 10 (N=228k)  # noqa: E501
-LH_ALL4  priority 3045: 200 > vtan >= 150 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10 (N=360k)  # noqa: E501
-LH_ALL5  priority 6092: vtan >= 200      and not (3 < M_G < 5) and 10 > parallax_over_error >= 5 (N=274k)  # noqa: E501)
-LH_ALL6  priority 6094: 200 > vtan >= 150 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5 (N=437k)  # noqa: E501
-
-Note: LH_MSTO5 priority 3030 Covers both vtan>200 and vtan>150.
 
     """
-
-    name = 'mwm_halo_local'
-    category = 'science'
-    instrument = None  # instrument set in post_process()
-    cadence = 'bright_1x1'
-    program = 'mwm_halo'
-    mapper = 'MWM'
-    priority = None  # priority set in post_process()
-    can_offset = True
 
     def build_query(self, version_id, query_region=None):
 
@@ -595,49 +651,49 @@ Note: LH_MSTO5 priority 3030 Covers both vtan>200 and vtan>150.
 
         return query
 
+
+class MWM_halo_local_vtanhigh_apogee_single_Carton(MWM_halo_local_Base_Carton):
+    """
+    mwm_halo_local_vtanhigh_apogee_single
+    vtan >= 150 and parallax_over_error >=10 OR
+    vtan >=150 and 3 < M_G < 5 and 10 > parallax_over_error > 5
+    G < 13
+    PRIORITY: 2981
+    CADENCE: bright_1x1
+    INSTRUMENT: APOGEE
+    can_offset = True
+    """
+    name = 'mwm_halo_local_vtanhigh_apogee_single'
+    category = 'science'
+    instrument = 'APOGEE'
+    cadence = 'bright_1x1'
+    program = 'mwm_halo'
+    mapper = 'MWM'
+    priority = 2981
+    can_offset = True
+
+    def build_query(self, version_id, query_region=None):
+
+        query = super().build_query(version_id, query_region)
+        query = query.where(Gaia_DR3.phot_g_mean_mag < 13)
+        return query
+
     def post_process(self, model):
-        """
-Priority: 11 levels
-vtan >= 200 and 3 < M_G < 5 and parallax_over_error >= 50: 2980
-200 > vtan >= 150 and 3 < M_G < 5 and parallax_over_error >= 50: 2985
-vtan >= 200 and not (3 < M_G < 5) and parallax_over_error >= 50: 2990
-200 > vtan >= 150 and not (3 < M_G < 5) and parallax_over_error >= 50: 2995
-vtan >= 200 and 3 < M_G < 5 and 50 > parallax_over_error >= 10: 3020
-200 > vtan >= 150 and 3 < M_G < 5 and 50 > parallax_over_error >= 10: 3025
-vtan >= 150 and 3 < M_G < 5 and 10 > parallax_over_error >= 5: 3030
-vtan >= 200 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10: 3040
-200 > vtan >= 150 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10: 3045
-vtan >= 200 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5: 6092
-200 > vtan >= 150 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5: 6094
 
-Priority algorithm: (exact numbers are TBA, this is based on v0.5 priorities)
-LH_MSTO1 priority 2980: vtan >= 200      and 3 < M_G < 5       and parallax_over_error >= 50 (N=14k)  # noqa: E501
-LH_MSTO2 priority 2985: 200 > vtan >= 150 and 3 < M_G < 5       and parallax_over_error >= 50 (N=24k)  # noqa: E501
-LH_ALL1  priority 2990: vtan >= 200      and not (3 < M_G < 5) and parallax_over_error >= 50 (N=28k)  # noqa: E501
-LH_ALL2  priority 2995: 200 > vtan >= 150 and not (3 < M_G < 5) and parallax_over_error >= 50 (N=42k)  # noqa: E501
-LH_MSTO3 priority 3020: vtan >= 200      and 3 < M_G < 5       and 50 > parallax_over_error >= 10 (N=98k)  # noqa: E501
-LH_MSTO4 priority 3025: 200 > vtan >= 150 and 3 < M_G < 5       and 50 > parallax_over_error >= 10 (N=179k)  # noqa: E501
-LH_MSTO5 priority 3030: vtan >= 150       and 3 < M_G < 5       and 10 > parallax_over_error >= 5 (N=12k)  # noqa: E501
-LH_ALL3  priority 3040: vtan >= 200      and not (3 < M_G < 5) and 50 > parallax_over_error >= 10 (N=228k)  # noqa: E501
-LH_ALL4  priority 3045: 200 > vtan >= 150 and not (3 < M_G < 5) and 50 > parallax_over_error >= 10 (N=360k)  # noqa: E501
-LH_ALL5  priority 6092: vtan >= 200      and not (3 < M_G < 5) and 10 > parallax_over_error >= 5 (N=274k)  # noqa: E501)
-LH_ALL6  priority 6094: 200 > vtan >= 150 and not (3 < M_G < 5) and 10 > parallax_over_error >= 5 (N=437k)  # noqa: E501
-
-Note: LH_MSTO5 priority 3030 Covers both vtan>200 and vtan>150.
-
-        Instrument: BOSS for G>13, APOGEE for G<13
-        """
+        cursor = self.database.execute_sql(
+            "update sandbox.temp_mwm_halo_local_vtanhigh_apogee_single " +
+            "set selected = false;")
 
         cursor = self.database.execute_sql(
             "select catalogid, vtan, " +
             "parallax_over_error, phot_g_mean_mag, parallax from " +
-            " sandbox.temp_mwm_halo_local ;")
+            " sandbox.temp_mwm_halo_local_vtanhigh_apogee_single ;")
 
         output = cursor.fetchall()
 
         for i in range(len(output)):
             current_catalogid = output[i][0]
-            current_vtan = output[i][1]
+            # current_vtan = output[i][1]
             current_parallax_over_error = output[i][2]
             current_phot_g_mean_mag = output[i][3]
             current_parallax = output[i][4]
@@ -646,87 +702,28 @@ Note: LH_MSTO5 priority 3030 Covers both vtan>200 and vtan>150.
             # that current_phot_g_mean_mag and current_parallax are not null.
             current_m_g = current_phot_g_mean_mag - (10 - 5 * math.log10(current_parallax))
 
-            m_g_3to5 = ((3 < current_m_g) and (current_m_g < 5))
-            parallax_over_error_10to50 = ((10 <= current_parallax_over_error) and
-                                          (current_parallax_over_error < 50))
+            is_m_g_3to5 = ((3 < current_m_g) and (current_m_g < 5))
 
-            current_priority = None
             # Below we do not check for vtan > 150 since
-            # the query in build_query() has vtan > 150
+            # the query in base class build_query() has vtan > 150
             #
             # We do not check for parallax_over_error > 5 since
-            # the query in build_query() has parallax_over_error > 5
-            #
-            # The order below is the same order as in the comment above.
-            if (m_g_3to5 and (current_parallax_over_error >= 50)):
-                if (current_vtan >= 200):
-                    current_priority = 2980
-                else:
-                    current_priority = 2985
+            # the query in bas class build_query() has parallax_over_error > 5
 
-            elif ((not m_g_3to5) and (current_parallax_over_error >= 50)):
-                if (current_vtan >= 200):
-                    current_priority = 2990
-                else:
-                    current_priority = 2995
-
-            elif (m_g_3to5 and parallax_over_error_10to50):
-                if (current_vtan >= 200):
-                    current_priority = 3020
-                else:
-                    current_priority = 3025
-
-            # See the case LH_MSTO5 in the above comments.
-            # This is the odd one out since it sets the
-            # same priority for both branches of if(current_vtan >= 200).
-            # We could remove the inside if() but we are keeping it
-            # so that the code is similar to the other elif sections.
-            elif (m_g_3to5 and (10 > current_parallax_over_error)):
-                if (current_vtan >= 200):
-                    current_priority = 3030
-                else:
-                    current_priority = 3030
-
-            elif ((not m_g_3to5) and parallax_over_error_10to50):
-                if (current_vtan >= 200):
-                    current_priority = 3040
-                else:
-                    current_priority = 3045
-
-            elif ((not m_g_3to5) and (10 > current_parallax_over_error)):
-                if (current_vtan >= 200):
-                    current_priority = 6092
-                else:
-                    current_priority = 6094
-
-            else:
-                raise TargetSelectionError('error MWM_halo_local_Carton: ' +
-                                           'post_process(): ' +
-                                           ' no priority assigned for target: ' +
-                                           'm_g=' + current_m_g + ', '
-                                           'parallax_over_error=' +
-                                           current_parallax_over_error + ',' +
-                                           'vtan=' + current_vtan)
-
-            if current_priority is not None:
+            if (is_m_g_3to5 and current_parallax_over_error < 10):
                 self.database.execute_sql(
-                    " update sandbox.temp_mwm_halo_local " +
-                    " set priority = '" + str(current_priority) + "'"
+                    " update sandbox.temp_mwm_halo_local_vtanhigh_apogee_single " +
+                    " set selected = true " +
                     " where catalogid = " + str(current_catalogid) + ";")
 
-            current_instrument = None
-            if (current_phot_g_mean_mag < 13):
-                current_instrument = 'APOGEE'
-            else:
-                current_instrument = 'BOSS'
-
-            if current_instrument is not None:
+            if (current_parallax_over_error >= 10):
                 self.database.execute_sql(
-                    " update sandbox.temp_mwm_halo_local " +
-                    " set instrument = '" + current_instrument + "'"
+                    " update sandbox.temp_mwm_halo_local_vtanhigh_apogee_single " +
+                    " set selected = true " +
                     " where catalogid = " + str(current_catalogid) + ";")
 
 
+#####################################
 class MWM_halo_local_dark_Carton(BaseCarton):
     """  5.1.37.  mwm_halo_local_dark
 Shorthand name: mwm_halo_local_dark
