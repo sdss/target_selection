8de1845010aaff1d8340f457637b511ff510e4b8
José Sánchez-Gallego <gallegoj@uw.edu>
2024-02-14 13:21:44 -0800

    Revert using rich logger

--- python/target_selection/__init__.py
+++ python/target_selection/__init__.py
@@ -13,7 +13,7 @@ config = get_config('target_selection',
                     os.path.join(os.path.dirname(__file__),
                                  'config/target_selection.yml'))
 
-log = get_logger(NAME, use_rich_handler=True)
+log = get_logger(NAME)
 
 
 __version__ = get_package_version(path=__file__,
