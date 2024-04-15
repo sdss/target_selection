09ecc0fb2a9540708de599775aaba4774f635d10
Zach Way <zachway1996@gmail.com>
2024-04-15 13:53:58 -0600

    Testing git rm --cached

--- docs/v1_documentation/catalog_to_catalog/run_catalogidx_to_catalogidy_ind_test.py
+++
@@ -1,7 +0,0 @@
-from create_catalogidx_to_catalogidy import MetaXMatch, create_unique_from_region
-from sdssdb.peewee.sdss5db.catalogdb import database
-
-metax = MetaXMatch('catalogidx_to_catalogidy_ind_test.yml', database)
-
-metax.run()
-create_unique_from_region("catalogidx_to_catalogidy_catalog_to_allstar_dr17_synspec_rev1")
