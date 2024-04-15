e91bf50bce6c85a0d0fe25a73ea99c2adf6faf30
Zach Way <zachway1996@gmail.com>
2024-04-15 13:57:00 -0600

    Removing unnecessary files

--- docs/v1_documentation/catalog_to_catalog/add_ra_dec_catid.sql
+++
@@ -1,6 +0,0 @@
-create table sandbox.sdss_id_flat_new as
-select sid.sdss_id, sid.catalogid, sid.version_id, sid.ra_sdss_id, sid.dec_sdss_id,
-       sid.n_associated, sid.pk, cat.ra as ra_catalogid, cat.dec as dec_catalogid
-from catalogdb.sdss_id_flat sid 
-join catalogdb.catalog cat
-on sid.catalogid=cat.catalogid;
