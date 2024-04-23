60f26daacbb48759c6daa4b5976882b4dbe554c6
Zachary Way <zway1@gsu.edu>
2024-04-23 14:15:25 -0400

    Fixed more linting issues

--- docs/v1_documentation/catalog_to_catalog/create_catalogidx_to_catalogidy.py
+++ docs/v1_documentation/catalog_to_catalog/create_catalogidx_to_catalogidy.py
@@ -18,10 +18,7 @@ from playhouse.postgres_ext import ArrayField
 
 from sdssdb.peewee.sdss5db.catalogdb import database, Catalog
 from sdssdb.peewee.sdss5db.targetdb import Target
-
-
 import target_selection
-
 
 database.connect(dbname='sdss5db', user="sdss_user")  # Use 'sdss' to get read/write permissions
 
