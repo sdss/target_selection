ed7874dfd420ad251e2b08da0b87ec285eb9a246
Zachary Way <zway1@gsu.edu>
2024-04-30 11:22:34 -0400

    Fixed __init__ imports

--- python/target_selection/sdss_id/__init__.py
+++ python/target_selection/sdss_id/__init__.py
@@ -1,1 +1,7 @@
-from create_catalogidx_to_catalogidy import 
+from append_to_sdss_id import (AppendToTables, SdssIdFlat, SdssIdFlatToAdd,
+                               SdssIdStacked, SdssIdStackedToAdd,
+                               TempCatalogidV21, TempCatalogidV25,
+                               TempCatalogidV31)
+from create_catalogidx_to_catalogidy import (MetaXMatch, TempMatch,
+                                             UniqueMatch,
+                                             create_unique_from_region)
