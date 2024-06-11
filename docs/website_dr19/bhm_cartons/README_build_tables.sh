2917b09cde530223850b798214019fc8d6fd35b5
Tom Dwelly <dwelly@mpe.mpg.de>
2024-06-10 17:14:09 -0700

    first cut of BHM plate era carton descriptions and code

--- docs/website_dr19/bhm_cartons/README_build_tables.sh
+++ docs/website_dr19/bhm_cartons/README_build_tables.sh
@@ -1,104 +1,126 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-# run some queries against the database, format the output, paste the result into 'Custom HTML' clock in wordpress.
+# this script is copied+adapted from the dr18 version, and may contain some redundant info.
+# In DR19 we just add the v0 bhm cartons that were used during the plate era
 
-# This gnerates the HTML for the tables of cartons on the following page:
-# https://testng.sdss.org/dr18/bhm/programs/cartons/
+# So most of this is DR18 related
+# run some queries against the database, format the output, paste the result into 'Custom HTML' block in wordpress.
+
+# This generates the HTML for the tables of cartons on the following page:
+# https://test.sdss.org/dr18/bhm/programs-dr19/cartons-dr19/
 
 #alias sdssdb='psql -h localhost -p 7502 -U sdss_user -d sdss5db'
 alias sdssdb='psql -h operations.sdss.org -U sdss_user -d sdss5db'
 
-Q="SELECT c.carton,c.target_selection_plan,v.tag,
-          CASE WHEN (c.carton ~ 'bhm_colr' OR c.carton ~ 'bhm_gua')  THEN 'ancillary'
-               WHEN (cc.program = 'bhm_filler' OR c.carton ~ 'bhm_csc')  THEN 'non-core'
-               WHEN (cc.program = 'bhm_spiders' AND (c.carton ~ '_supercosmos' OR c.carton ~ '_efeds_stragglers' OR c.carton ~ '_skymapperdr2' OR c.carton ~ '_gaiadr2' OR c.carton ~ '_sep'))  THEN 'non-core'
-               ELSE 'core' END as status,
-          cc.program
-FROM minidb.dr18_carton as c
-join carton as cc
-  on c.carton = cc.carton
-join targetdb.version as v
-  on cc.version_pk = v.pk
-where c.carton ~ 'bhm'
-  and v.plan = c.target_selection_plan
-order by c.carton;"
+WORKINGDIR=~/SDSSV/dr19/minidb
+cd $WORKINGDIR
 
-sdssdb -c "$Q" > q_result_v0.5.3.txt
-
-
-gawk -v gen='"v0.5.3"' 'BEGIN {
-printf("<figure class=\"wp-block-table is-style-stripes\">\n\
-<table>\n\
-<thead>\n\
-<tr>\
-<th>carton name</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">science <br>program</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>plan</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>tag</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">status</th>\
-</tr>\
-\n</thead>\n<tbody>\n");}
-$1~/^bhm_/{
-split($1,c,"_"); prog=c[2]; PROG=toupper(prog);
-if (prog~/gua|colr/) {prog="ancillary";PROG="Ancillary programs";};
-printf("<tr><td><a href=\"#%s_plan%s\" data-type=\"internal\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://testng.sdss.org/dr18/bhm/programs/%s\">BHM %s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://github.com/sdss/target_selection/tree/%s/\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td></tr>\n",
-$1, $3, $1, prog, PROG, $3, $5, $5, $7)}
-END {printf("</tbody></table><figcaption>A table of all BHM cartons from targeting generation %s</figcaption></figure>\n", gen)}' q_result_v0.5.3.txt > carton_table_block_generation_0.5.3.html
+if [[ "true" == "Ntrue" ]]; then
+    cd $WORKINGDIR
+    # No need to redo this for DR19
+    Q="SELECT c.carton,c.target_selection_plan,v.tag,
+              CASE WHEN (c.carton ~ 'bhm_colr' OR c.carton ~ 'bhm_gua')  THEN 'ancillary'
+                   WHEN (cc.program = 'bhm_filler' OR c.carton ~ 'bhm_csc')  THEN 'non-core'
+                   WHEN (cc.program = 'bhm_spiders' AND (c.carton ~ '_supercosmos' OR c.carton ~ '_efeds_stragglers' OR c.carton ~ '_skymapperdr2' OR c.carton ~ '_gaiadr2' OR c.carton ~ '_sep'))  THEN 'non-core'
+                   ELSE 'core' END as status,
+              cc.program
+    FROM minidb.dr19_carton as c
+    join carton as cc
+      on c.carton = cc.carton
+    join targetdb.version as v
+      on cc.version_pk = v.pk
+    where c.carton ~ 'bhm'
+      and v.plan = c.target_selection_plan
+    order by c.carton;"
+    
+    sdssdb -c "$Q" > q_result_v0.5.3.txt
 
 
+    gawk -v gen='"v0.5.3"' 'BEGIN {
+    printf("<figure class=\"wp-block-table is-style-stripes\">\n\
+    <table>\n\
+    <thead>\n\
+    <tr>\
+    <th>carton name</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">science <br>program</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>plan</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>tag</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">status</th>\
+    </tr>\
+    \n</thead>\n<tbody>\n");}
+    $1~/^bhm_/{
+    split($1,c,"_"); prog=c[2]; PROG=toupper(prog);
+    if (prog~/gua|colr/) {prog="ancillary";PROG="Ancillary programs";};
+    printf("<tr><td><a href=\"#%s_plan%s\" data-type=\"internal\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://testng.sdss.org/dr18/bhm/programs/%s\">BHM %s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://github.com/sdss/target_selection/tree/%s/\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td></tr>\n",
+    $1, $3, $1, prog, PROG, $3, $5, $5, $7)}
+    END {printf("</tbody></table><figcaption>A table of all BHM cartons from targeting generation %s</figcaption></figure>\n", gen)}' q_result_v0.5.3.txt > carton_table_block_generation_0.5.3.html
+    
+fi
 
 
-######################################
-## manually select the eFEDS plates cartons
 
-Q="SELECT cc.carton,v.plan,v.tag,
-          'non-core' as status,
-          cc.program
-FROM carton as cc
-join targetdb.version as v
-  on cc.version_pk = v.pk
-where
-  cc.carton ~ '-efeds'
-  and v.plan ~ '0.1.0'
-order by cc.carton;"
+if [[ "true" == "Ntrue" ]]; then
+    cd $WORKINGDIR
+    ######################################
+    ## manually select the BHM plates cartons
+    # check this logic when have db access
+    
+    Q="SELECT cc.carton,v.plan,v.tag,
+              CASE WHEN (c.carton ~ 'bhm_colr' OR c.carton ~ 'bhm_gua')  THEN 'ancillary'
+                   WHEN (cc.program = 'bhm_filler' OR c.carton ~ 'bhm_csc' OR c.carton ~ '-efeds')  THEN 'non-core'
+                   ELSE 'core' END as status,
+              cc.program
+    FROM carton as cc
+    join targetdb.version as v
+      on cc.version_pk = v.pk
+    where
+      cc.carton ~ 'bhm'
+      and v.plan ~ '0.1.0'
+    order by cc.carton;"
+    
+    sdssdb -c "$Q" > q_result_SDSSV_plates.txt
+    
+    
+    gawk -v gen='"SDSS-V plates"' 'BEGIN {
+    printf("<figure class=\"wp-block-table is-style-stripes\">\n\
+    <table>\n\
+    <thead>\n\
+    <tr>\
+    <th>carton name</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">science <br>program</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>plan</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>tag</th>\
+    <th class=\"has-text-align-center\" data-align=\"center\">status</th>\
+    </tr>\
+    \n</thead>\n<tbody>\n");}
+    $1~/^bhm_/{
+    split($1,c,"_"); prog=c[2]; PROG=toupper(prog);
+    if (prog~/gua|colr/) {prog="ancillary";PROG="Ancillary programs";};
+    printf("<tr><td><a href=\"#%s_plan%s\" data-type=\"internal\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://testng.sdss.org/dr18/bhm/programs/%s\">BHM %s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://github.com/sdss/target_selection/tree/%s/\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td></tr>\n",
+    $1, $3, $1, prog, PROG, $3, $5, $5, $7)}
+    END {printf("</tbody></table><figcaption>A table of all BHM cartons from targeting generation %s</figcaption></figure>\n", gen)}' q_result_SDSSV_plates.txt > carton_table_block_generation_SDSSV_plates.html
 
-sdssdb -c "$Q" > q_result_eFEDS_plates.txt
-
-
-gawk -v gen='"eFEDS plates"' 'BEGIN {
-printf("<figure class=\"wp-block-table is-style-stripes\">\n\
-<table>\n\
-<thead>\n\
-<tr>\
-<th>carton name</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">science <br>program</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>plan</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">target <br>selection <br>tag</th>\
-<th class=\"has-text-align-center\" data-align=\"center\">status</th>\
-</tr>\
-\n</thead>\n<tbody>\n");}
-$1~/^bhm_/{
-split($1,c,"_"); prog=c[2]; PROG=toupper(prog);
-if (prog~/gua|colr/) {prog="ancillary";PROG="Ancillary programs";};
-printf("<tr><td><a href=\"#%s_plan%s\" data-type=\"internal\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://testng.sdss.org/dr18/bhm/programs/%s\">BHM %s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td><td class=\"has-text-align-center\" data-align=\"center\"><a href=\"https://github.com/sdss/target_selection/tree/%s/\">%s</a></td><td class=\"has-text-align-center\" data-align=\"center\">%s</td></tr>\n",
-$1, $3, $1, prog, PROG, $3, $5, $5, $7)}
-END {printf("</tbody></table><figcaption>A table of all BHM cartons from targeting generation %s</figcaption></figure>\n", gen)}' q_result_eFEDS_plates.txt > carton_table_block_generation_eFEDS_plates.html
-
+fi
 
 
 ### now process the json
 # see: proc_carton_desc.py
 # e.g.
-cd ~/SDSSV/gitwork/target_selection_again/docs/website/bhm_cartons/
-python proc_carton_desc.py > bhm_target_cartons.html
+WEBBASE=~/SDSSV/gitwork/target_selection/docs/website_dr19/bhm_cartons/
+cd $WEBBASE
+
+HTMLOUT=${WEBBASE}/bhm_target_cartons.html
+python proc_carton_desc.py > $HTMLOUT
+
+echo "HTML output is here: $HTMLOUT"
 
 
-# Then do this auto conversion to latex:
+# Then do the auto conversion to latex:
 #
-TEXOUT=bhm_target_cartons.tex
-pandoc --from=html --to=latex --output=$TEXOUT bhm_target_cartons.html
+TEXOUT=${WEBBASE}/bhm_target_cartons.tex
+pandoc --from=html --to=latex --output=$TEXOUT $HTMLOUT
 
-# replace the references with bibtex equivalents:
+# replace the references with bibtex equivalents - need to revise this for DR19:
 
 perl -0777 -pi -e 's/\\href{https:\/\/ui.adsabs.harvard.edu\/abs\/2020ApJS..250....8L\/abstract}{Lyke\net al., 2020}/\\citealt{Lyke2020}/g'  $TEXOUT
 perl -0777 -pi -e 's/\\href{https:\/\/ui.adsabs.harvard.edu\/abs\/2014ApJ...785..104R\/abstract}{Rykoff\net al., 2014}/\\citealt{Rykoff2014}/g'  $TEXOUT
@@ -128,9 +150,10 @@ perl -0777 -pi -e 's/σ/\$\\sigma\$/g'  $TEXOUT
 perl -0777 -pi -e 's/→/\$\\rightarrow\$/g'  $TEXOUT
 
 # perl -0777 -pi -e 's///g'  $TEXOUT
-awk 'BEGIN {flag=1} NF==0 {flag=1} $0~/Catalogdb tables required/ {flag=0} flag==1 {print $0}' $TEXOUT >  temp.$TEXOUT
-mv temp.$TEXOUT  $TEXOUT
+awk 'BEGIN {flag=1} NF==0 {flag=1} $0~/Catalogdb tables required/ {flag=0} flag==1 {print $0}' $TEXOUT >  ${TEXOUT}.temp
+mv ${TEXOUT}.temp  $TEXOUT
 
 # checks (should result in zero rows)
 grep -A1 adsab $TEXOUT
 
+echo "Latex output is here: $TEXOUT"
