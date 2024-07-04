4482acef15ad7d322cf6bc39722835fb97fbd69b
José Sánchez-Gallego <gallegoj@uw.edu>
2024-07-03 19:23:56 -0700

    Fix Sphinx conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -20,8 +20,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
-from pkg_resources import parse_version
-
 from target_selection import __version__
 
 
@@ -34,35 +32,40 @@ from target_selection import __version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary',
-              'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax',
-              'sphinx.ext.intersphinx', 'sphinx_click.ext',
-              'sphinx_copybutton', 'myst_parser']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.intersphinx",
+    "sphinx_click.ext",
+    "sphinx_copybutton",
+    "myst_parser",
+]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
-source_parsers = {
-}
+source_parsers = {}
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = 'target_selection'
-copyright = '{0}, {1}'.format('2019', 'José Sánchez-Gallego')
-author = 'José Sánchez-Gallego'
+project = "target_selection"
+copyright = "{0}, {1}".format("2019", "José Sánchez-Gallego")
+author = "José Sánchez-Gallego"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-# The short X.Y version.
-version = parse_version(__version__).base_version
 # The full version, including alpha/beta/rc tags.
 release = __version__
 
@@ -76,11 +79,11 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-default_role = 'py:obj'
+default_role = "py:obj"
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 # add_function_parentheses = True
@@ -94,7 +97,7 @@ add_module_names = True
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
@@ -106,17 +109,19 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 # Intersphinx mappings
-intersphinx_mapping = {'python': ('https://docs.python.org/3.6', None),
-                       'astropy': ('http://docs.astropy.org/en/latest', None),
-                       'numpy': ('http://docs.scipy.org/doc/numpy/', None),
-                       'peewee': ('http://docs.peewee-orm.com/en/latest/', None),
-                       'sdssdb': ('http://sdssdb.readthedocs.org/en/latest/', None),
-                       'pandas': ('https://pandas.pydata.org/docs', None),
-                       'matplotlib': ('https://matplotlib.org/', None),
-                       'healpy': ('https://healpy.readthedocs.io/en/latest', None)}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.6", None),
+    "astropy": ("http://docs.astropy.org/en/latest", None),
+    "numpy": ("http://docs.scipy.org/doc/numpy/", None),
+    "peewee": ("http://docs.peewee-orm.com/en/latest/", None),
+    "sdssdb": ("http://sdssdb.readthedocs.org/en/latest/", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
+    "matplotlib": ("https://matplotlib.org/", None),
+    "healpy": ("https://healpy.readthedocs.io/en/latest", None),
+}
 
-autodoc_mock_imports = ['tkinter', 'sdssdb.peewee.sdss5db']
-autodoc_member_order = 'groupwise'
+autodoc_mock_imports = ["tkinter", "sdssdb.peewee.sdss5db"]
+autodoc_member_order = "groupwise"
 
 napoleon_use_rtype = False
 napoleon_use_ivar = True
@@ -134,21 +139,21 @@ copybutton_prompt_is_regexp = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = 'furo'
-html_title = 'target_selection'
-html_logo = '_static/sdssv_logo.png'
-html_favicon = './_static/favicon_sdssv.ico'
+html_theme = "furo"
+html_title = "target_selection"
+html_logo = "_static/sdssv_logo.png"
+html_favicon = "./_static/favicon_sdssv.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = '{0}pdoc'.format('target_selection')
+htmlhelp_basename = "{0}pdoc".format("target_selection")
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -157,15 +162,12 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
-
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -175,8 +177,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, '{0}.tex'.format(project), u'{0} Documentation'.format(project),
-     author, 'manual'),
+    (
+        master_doc,
+        "{0}.tex".format(project),
+        "{0} Documentation".format(project),
+        author,
+        "manual",
+    ),
 ]
 
 # -- Options for manual page output ---------------------------------------
@@ -184,8 +191,13 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'target_selection', u'{0} Documentation'.format(project),
-     [author], 1)
+    (
+        master_doc,
+        "target_selection",
+        "{0} Documentation".format(project),
+        [author],
+        1,
+    )
 ]
 
 # -- Options for Texinfo output -------------------------------------------
@@ -194,7 +206,13 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, project, u'{0} Documentation'.format(project),
-     author, project, 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        project,
+        "{0} Documentation".format(project),
+        author,
+        project,
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
