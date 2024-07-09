b212b43d8cc52997f7ea0c0eb6d0d95e4b43d15b
Pramod Gupta <psgupta@astro.washington.edu>
2024-07-08 18:03:40 -0600

    add mwm_snc_further_ext

--- python/target_selection/cartons/mwm_snc_further_ext.py
+++ python/target_selection/cartons/mwm_snc_further_ext.py
@@ -17,9 +17,10 @@ from sdssdb.peewee.sdss5db.catalogdb import (
 
 from target_selection.cartons import BaseCarton
 
+
 class MWM_snc_further_ext_Base_Carton(BaseCarton):
-    """ 
-    MWM_monitor_apogee_Base_Carton is a base carton.
+    """
+    MWM_snc_further_ext_Base_Carton is a base carton.
 
     Actual cartons are implemented as subclasses of MWM_SNC_further_ext_Base_Carton
     for different conditions in the WHERE clause.
@@ -30,30 +31,32 @@ class MWM_snc_further_ext_Base_Carton(BaseCarton):
     def build_query(self, version_id, query_region=None):
         ll = Gaia_DR3.l
         bb = Gaia_DR3.b
-    # Dense regions (Galactic plane, SMC, LMC).
-    gal_cut = (((ll <= 180) & (bb < (-0.139 * ll + 25)) & (bb > (0.139 * ll - 25)))
-               | ((ll > 180) & (bb > (-0.139 * ll + 25)) & (bb < (0.139 * ll - 25)))
-               | (fn.sqrt(fn.pow(ll - 303.2, 2) + 2 * fn.pow(bb + 44.4, 2)) < 5)
-               | (fn.sqrt(fn.pow(ll - 280.3, 2) + 2 * fn.pow(bb + 33.0, 2)) < 8))
-    cte = Gaia_DR3.select(Gaia_DR3.source_id)\
-            .where(
-        Gaia_DR3.parallax < 10, Gaia_DR3.parallax - Gaia_DR3.parallax_error > 2,
-        Gaia_DR3.phot_g_mean_mag + 5 * fn.log10(Gaia_DR3.parallax / 1000) + 5 >
-            19.125 - 0.03225 * (1000 / Gaia_DR3.parallax)
-        ).cte('plx_mag_cte', materialized=True)
 
-    query = (Gaia_DR3.select().join(CatalogToGaia_DR3).join_from(
-        Gaia_DR3, cte, on=(Gaia_DR3.source_id == cte.c.source_id)).where((
-            (Gaia_DR3.astrometric_excess_noise < 2) & 
-            (Gaia_DR3.ruwe < 1.2) &
-            (Gaia_DR3.phot_bp_rp_excess_factor >
-                 1 + 0.015 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)) &
-            (Gaia_DR3.phot_bp_rp_excess_factor <
-                 1.3 + 0.06 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)) &
-            gal_cut) | ~gal_cut).where(
-                    CatalogToGaia_DR3.version_id == version_id,
-                    CatalogToGaia_DR3.best >> True).with_cte(cte))
-    return query
+    # Dense regions (Galactic plane, SMC, LMC).
+        gal_cut = (((ll <= 180) & (bb < (-0.139 * ll + 25)) & (bb > (0.139 * ll - 25)))
+                   | ((ll > 180) & (bb > (-0.139 * ll + 25)) & (bb < (0.139 * ll - 25)))
+                   | (fn.sqrt(fn.pow(ll - 303.2, 2) + 2 * fn.pow(bb + 44.4, 2)) < 5)
+                   | (fn.sqrt(fn.pow(ll - 280.3, 2) + 2 * fn.pow(bb + 33.0, 2)) < 8))
+
+        cte = Gaia_DR3.select(Gaia_DR3.source_id)\
+                .where(
+            Gaia_DR3.parallax < 10, Gaia_DR3.parallax - Gaia_DR3.parallax_error > 2,
+            Gaia_DR3.phot_g_mean_mag + 5 * fn.log10(Gaia_DR3.parallax / 1000) + 5 >
+                19.125 - 0.03225 * (1000 / Gaia_DR3.parallax)
+            ).cte('plx_mag_cte', materialized=True)
+
+        query = (Gaia_DR3.select().join(CatalogToGaia_DR3).join_from(
+            Gaia_DR3, cte, on=(Gaia_DR3.source_id == cte.c.source_id)).where((
+                (Gaia_DR3.astrometric_excess_noise < 2) &
+                (Gaia_DR3.ruwe < 1.2) &
+                (Gaia_DR3.phot_bp_rp_excess_factor >
+                     1 + 0.015 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)) &
+                (Gaia_DR3.phot_bp_rp_excess_factor <
+                     1.3 + 0.06 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)) &
+                gal_cut) | ~gal_cut).where(
+                        CatalogToGaia_DR3.version_id == version_id,
+                        CatalogToGaia_DR3.best >> True).with_cte(cte))
+        return query
 
 
 class Openfibertargets_mwm_snc_further_ext_apogee_bright_Carton(MWM_snc_further_ext_Base_Carton):
@@ -134,7 +137,7 @@ class Openfibertargets_mwm_snc_further_ext_boss_Carton(MWM_snc_further_ext_Base_
     name = "openfibertargets_mwm_snc_further_ext_boss"
     category = "science"
     instrument = "BOSS"
-    cadence = None  # cadence is set in post_process() 
+    cadence = None  # cadence is set in post_process()
     program = "open_fiber"
     mapper = "MWM"
     priority = 6086
@@ -143,7 +146,7 @@ class Openfibertargets_mwm_snc_further_ext_boss_Carton(MWM_snc_further_ext_Base_
     def build_query(self, version_id, query_region=None):
         query = super().build_query(version_id, query_region)
 
-        query = query.where(Gaia_DR3.phot_g_mean_mag > 10, 
+        query = query.where(Gaia_DR3.phot_g_mean_mag > 10,
                             Gaia_DR3.phot_g_mean_mag < 20,
                             Gaia_DR3.radial_velocity.is_null())
 
