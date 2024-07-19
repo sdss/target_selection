68aad01a57e5a814ca1bd04ed263e726a12c9c03
Pramod Gupta <psgupta@astro.washington.edu>
2024-07-18 18:45:48 -0600

    edit mwm_snc.py

--- python/target_selection/cartons/mwm_snc.py
+++ python/target_selection/cartons/mwm_snc.py
@@ -135,28 +135,130 @@ class MWM_SNC_100pc_APOGEE_Carton(MWM_SNC_100pc_Carton):
 
 
 class MWM_SNC_100pc_BOSS_Carton(MWM_SNC_100pc_Carton):
-    """MWM SNC 100pc targets to be observed with BOSS."""
+    """MWM SNC 100pc targets to be observed with BOSS.
+
+    Shorthand name:  mwm_snc_100pc_boss
+    https://sdss-wiki.atlassian.net/wiki/spaces/OPS/pages/13707660/Cartons+for+post+v1
+
+    Existing carton code: https://github.com/sdss/target_selection/blob/main/python/target_selection/cartons/mwm_snc.py
+
+    Simplified Description of selection criteria :
+    Select everything within 100pc (i.e. parallax > 10mas) and Gaia-G
+    fainter than 10th mag. Add some constraints on astrometric excess noise
+    in crowded regions (Galactic plane, LMC, SMC, towards Galactic center).
+    For details, see here. Pseudo-code: For the pseduo-code, I will shorten
+    it and use the definition of "gal_cut" from the existing carton code.
+    Also, the red text below is what is being added to the target selection
+    compared to the original v1 carton. (parallax > 0) & (parallax -
+    parallax_error > 10) & (phot_g_mean_mag > 10) &
+    ((astrometric_excess_noise < 2) & (ll >= 20) & (ll <= 340) & gal_cut) |
+    ((astrometric_excess_noise < 2) & (ruwe < 1.2) & (1 + 0.015 *
+    (phot_bp_mean_mag - phot_rp_mean_mag)^2 < phot_bp_rp_excess_factor) &
+    (phot_bp_rp_excess_factor < 1.3 + 0.06 * (phot_bp_mean_mag -
+    phot_rp_mean_mag)^2) & (ll < 20 | ll > 340) & gal_cut) | ~gal_cut)
+
+    Return columns: Same columns as before + astrometric_excess_noise,
+    phot_bp_rp_excess_factor, ruwe, phot_bp_mean_mag , phot_rp_mean_mag
+
+    Metadata:
+    Priority: 1800
+    Cadence:  if G < 16, bright_2x1 ; if G > 16, dark_2x1
+    Instrument: BOSS
+    can_offset = True
+    Lead contact: Ilija Medan
+
+    """
 
     name = "mwm_snc_100pc_boss"
     category = "science"
     program = "mwm_snc"
     mapper = "MWM"
     instrument = "BOSS"
-    priority = 2705
+    priority = 1800
     can_offset = True
 
     def build_query(self, version_id, query_region=None):
+        ll = Gaia_DR3.l
+        bb = Gaia_DR3.b
+
+        # Dense regions (Galactic plane, SMC, LMC).
+        gal_cut = (
+            ((ll <= 180) & (bb < (-0.139 * ll + 25)) & (bb > (0.139 * ll - 25)))
+            | ((ll > 180) & (bb > (-0.139 * ll + 25)) & (bb < (0.139 * ll - 25)))
+            | (fn.sqrt(fn.pow(ll - 303.2, 2) + 2 * fn.pow(bb + 44.4, 2)) < 5)
+            | (fn.sqrt(fn.pow(ll - 280.3, 2) + 2 * fn.pow(bb + 33.0, 2)) < 8)
+        )
+
         query = super().build_query(version_id, query_region)
-        query = query.where(Gaia_DR3.phot_g_mean_mag > 10)
+        query = query.where(
+            Gaia_DR3.parallax > 0,
+            Gaia_DR3.parallax - Gaia_DR3.parallax_error > 10,
+            Gaia_DR3.phot_g_mean_mag > 10,
+            ((Gaia_DR3.astrometric_excess_noise < 2) & (ll >= 20) & (ll <= 340) & gal_cut)
+            | (
+                (Gaia_DR3.astrometric_excess_noise < 2)
+                & (Gaia_DR3.ruwe < 1.2)
+                & (
+                    1 + 0.015 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)
+                    < Gaia_DR3.phot_bp_rp_excess_factor
+                )
+                & (
+                    Gaia_DR3.phot_bp_rp_excess_factor
+                    < 1.3 + 0.06 * fn.pow(Gaia_DR3.phot_bp_mean_mag - Gaia_DR3.phot_rp_mean_mag, 2)
+                    & (ll < 20 | ll > 340)
+                    & gal_cut
+                )
+                | ~gal_cut
+            ),
+        )
 
         return query
 
     def post_process(self, model, **kwargs):
-        # G < 16 => cadence = bright_flexible_1x2
-        model.update(cadence="bright_flexible_1x2").where(model.phot_g_mean_mag < 16).execute()
+        # G < 16 => cadence = bright_2x1
+        model.update(cadence="bright_2x1").where(model.phot_g_mean_mag < 16).execute()
 
-        # G > 16 => cadence = dark_flexible_1x2
-        model.update(cadence="dark_flexible_1x2").where(model.phot_g_mean_mag > 16).execute()
+        # G > 16 => cadence = dark_2x1
+        model.update(cadence="dark_2x1").where(model.phot_g_mean_mag >= 16).execute()
+
+        return model
+
+
+class MWM_SNC_100pc_BOSS_single_Carton(MWM_SNC_100pc_BOSS_Carton):
+    """MWM SNC 100pc targets to be observed with BOSS.
+
+    Shorthand name:  mwm_snc_100pc_boss_single
+    https://sdss-wiki.atlassian.net/wiki/spaces/OPS/pages/13707660/Cartons+for+post+v1
+    Simplified Description of selection criteria :
+    NOTE this is a subcarton of mwm_snc_100pc_boss (see above) where the only change
+    is the cadence and priority.
+
+    Metadata:
+    Priority: 1801
+    Cadence:  if G < 16, bright_1x1 ; if G > 16, dark_1x1
+    Instrument: BOSS
+    can_offset = True
+    Lead contact: Ilija Medan
+    """
+
+    name = "mwm_snc_100pc_boss_single"
+    category = "science"
+    program = "mwm_snc"
+    mapper = "MWM"
+    instrument = "BOSS"
+    priority = 1801
+    can_offset = True
+
+    def build_query(self, version_id, query_region=None):
+        query = super().build_query(version_id, query_region)
+        return query
+
+    def post_process(self, model, **kwargs):
+        # G < 16 => cadence = bright_1x1
+        model.update(cadence="bright_1x1").where(model.phot_g_mean_mag < 16).execute()
+
+        # G > 16 => cadence = dark_1x1
+        model.update(cadence="dark_1x1").where(model.phot_g_mean_mag >= 16).execute()
 
         return model
 
